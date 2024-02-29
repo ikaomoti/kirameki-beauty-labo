25d95e3b7635015ab1de5447dd511c4e3a0eee5f
ikaomoti <ikaomoti@gmail.com>
2024-02-29 19:15:52 +0900

    1回目のコミット

--- asset/script/main.js
+++ asset/script/main.js
@@ -3,15 +3,15 @@ const swiper = new Swiper(".swiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
-    slidesPerView: 1.25, //表示するスライド数
+    slidesPerView: 1.25, 
     centeredSlides: true,
-    spaceBetween: 16,//スライド間の幅(px)
+    spaceBetween: 16,
     breakpoints: {
-        768: {  //768px以上のとき
-            slidesPerView: 3,//表示するスライド数
-            slidesPerGroup: 1,//一度にスライドする数
-            spaceBetween: 15,//スライド間の幅(px)
+        768: {  
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+            spaceBetween: 15,
         },
     },
-    speed: 600 //スライドの推移時間を600msに
+    speed: 600 
 });
